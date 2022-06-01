cb1b7e89cee87483daf58177516d737479187fac
334268034 <334268034@C-2UA40815D9.tdsb.on.ca>
2022-06-01 13:26:26 -0400

    Added ship methods

--- BattleShip/src/Ship.java
+++ BattleShip/src/Ship.java
@@ -3,10 +3,29 @@ public class Ship {
 	
 	private Cell[] parts;//the cells in which a ship is made 
 	private int length;
+	private boolean isSunk = false;
 	
 	//constructor 
 	public Ship(int length,Cell[] cells) {
 		cells = parts;
 		this.length=length;
 	}
+	
+	public boolean getSunk() {
+		return isSunk;
+	}
+	
+	public void setSunk(boolean sunk) {
+		isSunk = sunk;
+	}
+	
+	private boolean checkSunk() {
+		isSunk = true;
+		for(Cell part : parts) {
+			if(part.isHit() == false) {
+				isSunk = false;
+			}
+		}
+	}
+	
 }
