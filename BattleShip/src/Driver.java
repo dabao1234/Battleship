ec5c610d0db7b81aea201504ffa2982672e3c24e
334174778 <334174778@C-2UA5511Q4C.tdsb.on.ca>
2022-06-02 14:03:50 -0400

    Driver Update

--- BattleShip/src/Driver.java
+++ BattleShip/src/Driver.java
@@ -1,16 +1,38 @@
-
+import java.util.Scanner;
 public class Driver {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner in = new Scanner(System.in);
+		boolean programFinished = false;
 		Board b=new Board();
-
-		b.placeShipHorizontall(0, 2, 5);
-		b.display();
-		b.placeShipVertical(0, 2, 5);
-		b.display();
-
 		
+		
+		
+		b.display();
+		while(!programFinished) {
+			//Section to place the ship 
+			b.display();
+			break;
+		}
+		
+	}
+	/**
+	 * 
+	 * @param in
+	 * @return
+	 */
+	public static int getRow(Scanner in) {
+		return in.nextInt();
+	}
+	
+	/**
+	 * 
+	 * @param in
+	 * @return
+	 */
+	public static int getCol(Scanner in) {
+		return in.nextInt();
 	}
 
 }
