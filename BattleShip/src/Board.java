3c1ce2b5b44b4ccd7d21105a0253831c332e6810
334220985 <334220985@C-2UA5432HWT.tdsb.on.ca>
2022-06-06 14:22:26 -0400

    Fixed place ship method

--- BattleShip/src/Board.java
+++ BattleShip/src/Board.java
@@ -37,12 +37,15 @@ public class Board {
 
 		Cell[] cell = new Cell[shipLength];
 		boolean isEmpty = false;
-		isEmpty = checkOverlapVertical(startrow, startcol, shipLength);// checks around the ship
+		isEmpty = checkAroundVertical(startrow, startcol, shipLength);// checks around the ship
 		System.out.println(isEmpty);
 		if (isEmpty == true) {// if it is empty place ship
 			for (int i = 0; i < shipLength; i++) {
 				board[startrow+i][startcol].setState(Cellstate.ship);
 			}
+		}
+		else {
+			System.out.println("error");
 		}
 
 		Ship newShip = new Ship(shipLength, cell);
@@ -63,6 +66,9 @@ public class Board {
 				// ADD THESE CELLS TO THE SHIP OBJECT
 			}
 		}
+		else {
+			System.out.println("error");
+		}
 	}
 
 	public boolean hasShip(int row, int col) {
@@ -80,15 +86,9 @@ public class Board {
 			return false;
 		}
 	}
-<<<<<<< HEAD
 
 	// sets the enum of all ships to the state
-=======
-	
-	// makes every cell invisible, so ships will display as blank 
->>>>>>> branch 'main' of https://github.com/dabao1234/Battleship
 	public void hideShips() {
-<<<<<<< HEAD
 
 	}
 
@@ -98,74 +98,94 @@ public class Board {
 	 * @param startcol where the ship starts column
 	 * @return
 	 */
-	public boolean checkOverlapVertical(int startrow, int startcol, int shiplength) {
-
-		try {
+	public boolean checkAroundVertical(int startrow, int startcol, int shiplength) {
+		
+			int col=startcol;
+			int row=startrow;
+			
+			
 			// checks vertical overlap
-			for (int i = 0; i < shiplength + 2; i++) {// plus two as it needs to check above and below ship length
-				if (board[startrow + 1][startcol].equals(Cellstate.ship)) {
+			for (int i = 0; i < shiplength; i++) {// plus two as it needs to check above and below ship length
+				try {
+				if ((board[row + i][col].getState()).equals(Cellstate.ship)) {
 					return false;
+				}
+				}catch(Exception e) {
+					
 				}
 			}
 
 			// checks to the left
-			startrow = startrow - 1;
-			startcol = startcol - 1;// this happens os it will start the colloum below start
-			// checks the row to the left
+			row = startrow - 1;
+			System.out.println(row);
+			col = startcol - 1;// this happens os it will start the colloum below start
 			for (int i = 0; i < shiplength + 2; i++) {// plus two as it needs to check above and below ship length
-				if (board[startrow + i][startcol].equals(Cellstate.ship)) {
+				try {
+				if ((board[row + i][col].getState()).equals(Cellstate.ship)) {
 					return false;
+				}
+				}catch(Exception e) {
+					
+				}
+			}
+			col = startcol + 1;// checks to the right of the place
+			for (int i = 0; i < shiplength + 2; i++) {// plus two as it needs to check above and below ship length
+				try {
+				if ((board[startrow + i][col].getState()).equals(Cellstate.ship)) {
+					return false;
+				}
+				}catch(Exception e) {
+					
 				}
 			}
 
-			for (int i = 0; i < shiplength + 2; i++) {// plus two as it needs to check above and below ship length
-				if (board[startrow + 1][startcol].equals(Cellstate.ship)) {
-					return false;
-				}
-			}
-
-			startcol = startcol + 1;// checks to the right of the place
-		} catch (Exception e) {
-			System.out.println("out of bounds");
-			return false;
-		}
+			
+	
+	
 
 		return true;
-=======
-		for (int i = 0; i < 10; i++) {
-			for (int j = 0; j < 10; j++) {
-				board[i][j].setVisible(false);
-			}
-		}
->>>>>>> branch 'main' of https://github.com/dabao1234/Battleship
 	}
 	
 	public boolean checkOverlapHorizontal(int startrow, int startcol, int shiplength) {
 		//checks above 
-		startrow=startrow-1;
-		startcol=startcol-1;//always starts one col behid 
+		int row=startrow;
+		int col=startcol;
+		row=startrow-1;
+		col=startcol-1;//always starts one col behid 
+		
 		for(int i=0;i<shiplength+2;i++) {
-			if(board[startrow][startcol+i].equals(Cellstate.ship)) {
+		
+			try {
+			
+			if((board[row][col+i].getState()).equals(Cellstate.ship)) {
 				return false;
+			}
+			}catch(Exception e) {
+				System.out.println("error");
 			}
 		}
 		
-		//checks mid]
-		startrow=startrow+1;//resets start row 
+		//checks mid
+		row=row+1;//resets start row 
 		for(int i=0;i<shiplength+2;i++) {
-			if(board[startrow][startcol+i].equals(Cellstate.ship)) {
+			if((board[row][startcol+i].getState()).equals(Cellstate.ship)) {
 				return false;
 			}
 		}
 		
 		//checks below 
 		
-		startrow=startrow+1;//goes below
+		row=startrow+1;//goes below
 		for(int i=0;i<shiplength+2;i++) {
-			if(board[startrow][startcol+i].equals(Cellstate.ship)) {
+			try {
+			if(board[row][col+i].getState().equals(Cellstate.ship)) {
 				return false;
 			}
+			}catch(Exception e) {
+				
+			}
 		}
+		
 		return true;
 	}
 	
@@ -183,50 +203,19 @@ public class Board {
 			return false;
 		}
 	}
-<<<<<<< HEAD
 
 	public void shoot(int row, int col) {
-=======
-	
-	/**
-	 * 
-	 * @param row
-	 * @param col
-	 * @return whether or not the shot has succeeded
-	 */
-	
-	public boolean shoot(int row, int col) {
->>>>>>> branch 'main' of https://github.com/dabao1234/Battleship
 		// checks to see if there's a ship there
 		if (hasShip(row, col) == true) {
 			// checks to see if the ship has been hit already
-<<<<<<< HEAD
 			if (!isHit(row, col) == true) {
-=======
-			if(!isHit(row, col) == true) {
-				// if not, make it a hit
->>>>>>> branch 'main' of https://github.com/dabao1234/Battleship
 				board[row][col].setState(Cellstate.hit);
-<<<<<<< HEAD
 			} else {
-=======
-			}
-			else {
-				// if so, we want them to enter another thing
->>>>>>> branch 'main' of https://github.com/dabao1234/Battleship
 				System.out.println("Bad");
-				return false;
 			}
-<<<<<<< HEAD
 		} else {
-=======
-		}
-		// if no ship there, make it a miss
-		else {
->>>>>>> branch 'main' of https://github.com/dabao1234/Battleship
 			board[row][col].setState(Cellstate.miss);
 		}
-		return true;
 	}
 
 	// public boolean isSunk() {
@@ -241,8 +230,7 @@ public class Board {
 				System.out.print(board[i][j] + " ");
 
 			}
-			// go to the next line for the next row of ships
-			System.out.println();
+			System.out.println();// everytime row increase go down
 		}
 	}
 }
