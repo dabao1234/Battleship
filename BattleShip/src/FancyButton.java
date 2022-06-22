392cbf9790c9537696712703ad8b4c1858da8032
caitroll <88864897+caitroll@users.noreply.github.com>
2022-06-21 22:26:19 -0400

    Commented
    Why do I have to do legit all the comments right at the end I literally had nothing to do with this class you know

--- BattleShip/src/FancyButton.java
+++ BattleShip/src/FancyButton.java
@@ -1,22 +1,37 @@
 import javafx.scene.control.Button;
 
+/**
+ * This class makes it so that we can give buttons locations
+ */
 public class FancyButton extends Button {
+	// Initializing the row and column of the button
 	private int col;
 	private int row;
-	public FancyButton(String s, int row,int coll) {
+	
+	/**
+	 * Constructor to create a button with 
+	 * @param s - what should be displayed
+	 * @param row - the row of the button
+	 * @param coll - the column of the button
+	 */
+	public FancyButton(String s, int row, int coll) {
 		super(s);
 		col = coll;
 		this.row=row;
 	}
 
 	/**
-	 * 
-	 * @return the coll
+	 * Gets the column of the button
+	 * @return the column
 	 */
 	public int getCol() {
 		return col;
 	}
 	
+	/**
+	 * Gets the row of the button
+	 * @return the row
+	 */
 	public int getRow() {
 		return row;
 	}
