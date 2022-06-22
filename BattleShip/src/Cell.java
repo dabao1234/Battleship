5e9f15574b23c998f4c852ad0ce7c50944272fd5
caitroll <88864897+caitroll@users.noreply.github.com>
2022-06-21 22:22:18 -0400

    Commented the cell class

--- BattleShip/src/Cell.java
+++ BattleShip/src/Cell.java
@@ -1,41 +1,75 @@
+/**
+ * One piece of the board that the players can shoot at and place ships on
+ */
 public class Cell {
+	// Each cell will have a state
 	private Cellstate state;
+	
+	// Each cell will have a location on the board
 	private int row;
 	private int col;
+	
+	// Each cell will have a current visibility
 	private boolean isVisible = true;
 	
-	//always empty unless told otherwise 
+	/**
+	 * Creates the cell object. By default it will be empty
+	 * @param row - the row the cell is in
+	 * @param column - the column the cell is in
+	 */
 	public Cell(int row, int column) {
 		state = Cellstate.empty;
 		col = column;
 		this.row = row;
 	}
-	// sets the state
+	
+	/**
+	 * Sets the state of the cell
+	 * @param state - the desired state
+	 */
 	public void setState(Cellstate state) {
 		this.state = state;
 	}
 
-	// gets state
+	/**
+	 * The current state of the cell
+	 * @return the cellstate
+	 */
 	public Cellstate getState() {
 		return state;
 	}
-	//gets row
+	
+	/**
+	 * Gets the cell's row
+	 * @return the row the cell is in
+	 */
 	public int getRow() {
 		return row;
 	}
-	//gets column
+	
+	/**
+	 * Gets the cell's column
+	 * @return the column the cell is in
+	 */
 	public int getCol() {
 		return col;
 	}
 	
-	// checks to see if the cell has been hit
+	/**
+	 * Checks to see if the cell has a hit ship
+	 * @return whether or not it has a hit ship
+	 */
 	public boolean isHit() {
 		if(state.equals(Cellstate.hit)) {
 			return true;
 		}
 		return false;
 	}
-	//checks to see if the cell is a miss
+	
+	/**
+	 * Checks to see if the cell has a missed shot
+	 * @return whether or not the cell is a miss
+	 */
 	public boolean isMiss() {
 		if(state.equals(Cellstate.miss)) {
 			return true;
@@ -43,15 +77,18 @@ public class Cell {
 		return false;
 	}
 	
-	// sets the visibility of the cell which will determine whether the ships will be
-	// displayed or not
+	/**
+	 * sets the visibility of the cell which will determine whether any ship on it will be displayed or not
+	 * @param t - whether or not it is visible
+	 */
+	// 
 	public void setVisible(boolean t) {
 		isVisible = t;
 	}
-	
-	
 
-	// To string
+	/**
+	 * Displays the cell
+	 */
 	@Override
 	public String toString() {
 		// checks to see if the ships should be visible
@@ -70,7 +107,7 @@ public class Cell {
 					return "-";
 			}
 		}
-		// otherwise, doesn't display the ships
+		// if not, doesn't display any ships
 		else {
 			switch (state) {
 			case miss:
@@ -79,6 +116,7 @@ public class Cell {
 				return "X";
 			case empty:
 				return "-";
+			// Unhit ships appear as empty spaces
 			case ship:
 				return "-";
 			default:
