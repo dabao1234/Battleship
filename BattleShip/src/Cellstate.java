eb3099375b8ffb6b85d8e2603c353759a0737089
caitroll <88864897+caitroll@users.noreply.github.com>
2022-06-21 22:22:47 -0400

    Commented CellState

--- BattleShip/src/Cellstate.java
+++ BattleShip/src/Cellstate.java
@@ -1,4 +1,8 @@
+/**
+ * Holds the current state of the cell it belongs to
+ */
 
 public enum Cellstate {
+	// It will either have an unhit ship, a hit ship, a missed shot, or nothing
 	ship, hit, miss, empty
 }
