b6fd4866fa8f87b353bcab9895fe1238325851be
caitroll <88864897+caitroll@users.noreply.github.com>
2022-06-21 22:27:22 -0400

    Commented

--- BattleShip/src/Ship.java
+++ BattleShip/src/Ship.java
@@ -1,10 +1,15 @@
+/**
+ * Object made up of a number of cells that will appear on the board for the players to attempt to sink.
+ */
 
 public class Ship {
 	
-	private Cell[] parts;//the cells in which a ship is made 
+	private Cell[] parts; // Array of the cells the ship is made of
 	private int length;
+	// Whether or not the ship has sunk
 	private boolean isSunk = false;
-	private Board playerBoard;
+	
+	// Vertical or horizontal
 	private String orientation;
 	
 	/**
@@ -51,6 +56,31 @@ public class Ship {
 	}
 	
 	/**
+	 * Checks to see if a ship has been hit, but not sunk
+	 * @param b - the board the ship is located on
+	 * @return whether or not the ship has been hit but not sunk
+	 */
+	public boolean checkHit(Board b) {
+		
+		// Returning false if it has sunk already
+		if(getSunk(b)) {
+			return false;
+		}
+		
+		// Setting to false as default
+		boolean hit = false;
+		
+		// Looping through its parts
+		for(Cell part : parts) {
+			// If any part has been hit, hit is true
+			if(part.getState().equals(Cellstate.hit)) {
+				hit = true;
+			}
+		}
+		return hit;
+	}
+	
+	/**
 	 * Gets the cells in the ship
 	 * @return the array of cells the ship is comprised of
 	 */
@@ -67,6 +97,22 @@ public class Ship {
 	}
 	
 	/**
+	 * Finds a cell in the ship that has not been hit yet
+	 * @return an unhit cell
+	 */
+	public Cell notHit() {
+		// Loops through all the ship parts
+		for(Cell part : parts) {
+			// When it finds an unhit part, returning it
+			if(!part.getState().equals(Cellstate.hit)) {
+				return part;
+			}
+		}
+		// This will not execute, as the method is only called when the ship has not sunk yet
+		return null;
+	}
+	
+	/**
 	 * Checks if each cell in the ship has been hit to determine if it is sunk
 	 * @param b - the board the ship belongs to
 	 * @return whether or not it's been sunk
@@ -75,9 +121,10 @@ public class Ship {
 		isSunk = true;
 		//Loops through each part of the ship and checks if it is hit
 		for(Cell part : parts) {
-
+			// Finding the state of the cells
 			String state = part.toString();
-
+			
+			// If it's not hit, the ship has not sunk yet
 			if(!state.equals("X")) {
 				isSunk = false;
 			}
